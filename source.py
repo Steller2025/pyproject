29b50406b128d6a84cfc6348df0f7f5b39a9a0ae
aryan <aryan.shrestha.033@gmail.com>
2026-03-31 18:56:01 +0545

    projectsave1

---
+++ source.py
@@ -0,0 +1,7 @@
+# THIS IS WHERE WE WILL WRITE CODE
+# lets complete our assignment this week by next tuesday and learn 
+# till filehandling then we will begin and try to complete in the reamaing time
+#preferably 3 days then lasting if time remains we  could try to implement that library 
+#for 10 extra points  
+# make sure to clone my repository and remote connect this repositty to your local 
+# repository 
